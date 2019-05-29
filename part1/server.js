790e9df3bdfd6cb17efa4b160671c8804b2a48f8
Ryan Jones <ryanjones33@gmail.com>
2019-05-29 14:27:57 -0700

    Add part1 and part2 challenges

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+const path = require('path');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'))
 });
+
+app.get('/hello', function(req, res) {
+  res.send('Hello!');
+});
+
+app.get('/verify/:age', function(req, res) {
+  const age = req.params.age;
+  if (age > 13) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
+})
+app.post('/create/:name', function(req, res) {
+  res.json({ "id": 1, "name": req.params.name})
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,56 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.post('/create/:name/:age', function(req, res) {
+  const obj = {
+    name: req.params.name,
+    age: req.params.age
+  };
+  fs.readFile('./storage.json', function (err, data) {
+    let json = [];
+    if (data.length > 0) {
+      json = JSON.parse(data);
+    }
+    obj.id = json.length + 1;
+    json.push(obj);
+    fs.writeFileSync("./storage.json", JSON.stringify(json));
+  });
+  res.sendStatus(200);
 });
+
+app.get('/', function(req, res) {
+  fs.readFile('./storage.json', 'utf-8', function (err, data) {
+    if (data.length > 0) {
+      const json = JSON.parse(data);
+      res.json(json);
+    } else {
+      res.sendStatus(400);
+    }
+    // res.json(json);
+  });  
+});
+
+app.get('/:id', function(req, res) {
+  const id = parseInt(req.params.id);
+  fs.readFile('./storage.json', 'utf-8', function(err, data) {
+    if (data.length > 0) {
+      const json = JSON.parse(data);
+      const result = json.filter(person => {
+        return person.id === id;
+      })
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.sendStatus(400);
+      }
+    } else {
+      res.sendStatus(400)
+    }
+  });
+});
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
